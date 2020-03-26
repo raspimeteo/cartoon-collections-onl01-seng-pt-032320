1a7ab1a8c7b6c41ef49d73bfb264a9228fd87dd3
John Zonneveld <jjczon@gmail.com>
2020-03-26 22:29:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,7 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   array.map{|string| string.capitalize}
+  array.map{|string| string.join('!')
 end
 
 def long_planeteer_calls# code an argument here
